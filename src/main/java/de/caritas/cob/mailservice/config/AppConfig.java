d69bc1f09167e9675f8a48b128e465536c1fb7bd
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-07-20 15:56:30 +0200

    fix: test

--- src/main/java/de/caritas/cob/mailservice/config/AppConfig.java
+++ src/main/java/de/caritas/cob/mailservice/config/AppConfig.java
@@ -38,12 +38,6 @@ public class AppConfig {
     javaMailSender.setHost(host);
     javaMailSender.setPort(port);
 
-    Properties props = javaMailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
-
     return javaMailSender;
   }
 
