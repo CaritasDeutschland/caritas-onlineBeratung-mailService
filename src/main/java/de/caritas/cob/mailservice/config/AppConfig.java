0f96b00d33888bc01d84d64429cbe18f2a1829c5
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-24 14:16:17 +0200

    chore: use spring boot provided JavaMailSender, override mailsender config just for actuator tests

--- src/main/java/de/caritas/cob/mailservice/config/AppConfig.java
+++ src/main/java/de/caritas/cob/mailservice/config/AppConfig.java
@@ -1,15 +1,10 @@
 package de.caritas.cob.mailservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import java.util.Properties;
 
 /**
  * Contains some general spring boot application configurations
@@ -30,25 +25,4 @@ public class AppConfig {
     validatorFactoryBean.setValidationMessageSource(messageSource);
     return validatorFactoryBean;
   }
-
-  @Bean("emailsender") // need to define it for spring-actuator
-  public JavaMailSender getJavaMailSender(@Value("${spring.mail.host}") String host,
-      @Value("${spring.mail.port}") int port,
-      @Value("${spring.mail.username}") String username,
-      @Value("${spring.mail.password}") String password) {
-    final JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-    javaMailSender.setHost(host);
-    javaMailSender.setPort(port);
-
-    javaMailSender.setUsername(username);
-    javaMailSender.setPassword(password);
-
-    Properties props = javaMailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-
-    return javaMailSender;
-  }
-
 }
